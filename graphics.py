378c0309f31e5e5842c6a4c68074e5acc51aa76a
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-02-16 20:10:16 +0100

    Kutno - Implementaation of cell class, line class

--- graphics.py
+++ graphics.py
@@ -5,8 +5,8 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Runner")
-        self.__canvas = Canvas()
-        self.__canvas.pack()
+        self.__canvas = Canvas(self.__root, width=width, height=height, background="white")
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running_window = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -22,3 +22,100 @@ class Window:
     def close(self):
         self.__running_window = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color=fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, x2, y1, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "black")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(x2, y2), Point(x1, y2))
+            self._win.draw_line(line, "black")
+        if self.has_left_wall:
+            line = Line(Point(x1, y2), Point(x1, y1))
+            self._win.draw_line(line, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        # color = "red"
+        # if undo:
+        #     color = "gray"
+        # line = Line(Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2),
+        #      Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2))
+        # self._win.draw_line(line, color)
+
+        if self._win is None:
+            return
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        # moving left
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving up
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from graphics import Window
+from graphics import Window, Line, Point, Cell
 
 win = Window(800, 600)
+# line = Line(Point(50, 50), Point(400, 400))
+cell = Cell(win)
+cell1 = Cell(win)
+# win.draw_line(line, "black")
+cell.draw(0, 50, 0, 50)
+cell1.draw(50, 100, 50, 100)
+cell.draw_move(cell1)
 win.wait_for_close()
 
