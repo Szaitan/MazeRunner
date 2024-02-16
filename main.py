4ca8444d0c0153463bac8addec1459239f5bbe05
michal.krepiniewicz <m.krepiniewicz@gmail.com>
2024-02-16 20:14:01 +0100

    Kutno - Seperate page for cell class

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ win = Window(800, 600)
 cell = Cell(win)
 cell1 = Cell(win)
 # win.draw_line(line, "black")
-cell.draw(0, 50, 0, 50)
-cell1.draw(50, 100, 50, 100)
-cell.draw_move(cell1)
 win.wait_for_close()
 
